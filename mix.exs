cba24b5c2bf2ba32d255184622aa6e22f63c2530
Charles Bernasconi <charles.bernasconi@xing.com>
2024-03-06 12:31:29 +0100

    Upgrade deps

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule CredoPureCheck.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.5"},
+      {:credo, "~> 1.7.5"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
